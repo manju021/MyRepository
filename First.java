647e87b4007aec2a74c54996ebdf06449bc4d2ae
manju021 <manjusoni021@gmail.com>
2019-04-30 11:16:48 +0530

    MyRepository

--- First.java
+++ First.java
@@ -1,6 +1,15 @@
 class First
 {
+
+int a=10,b=20,c;
+c=a+b;
 public static void main(String a[])
 {
 System.out.println("hello");
+
+System.out.println("Good morning all");
+
+System.out.println(c);
+
+
 }}
